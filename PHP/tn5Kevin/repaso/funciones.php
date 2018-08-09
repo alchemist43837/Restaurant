fad10d2a8abd94103495146a3bf278167c37f1be
alchemist43837 <freddy_castaneda@hotmail.com>
2018-08-08 21:39:27 -0300

    avance de clase2

---
+++ PHP/tn5Kevin/repaso/funciones.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+
+function validar($data){
+  $name = trim ($data);
+}
+if ($email==''){
+  $errores
+}
+
+function guardarUsuario($data){
+$usuario=[
+  "name"=>$data['name'],
+  "email"=>$data['email'],
+  "pais"=>$data['pais'],
+  "pass"=>password_hash($data['pass'],PASSWORD_DEFAULT];
+]
+
+var_dump($usurio);
+
+}
+ ?>
---
+++ PHP/tn5Kevin/repaso/repaso.php
@@ -0,0 +1,1 @@
+<!-- filter_var , recibe la variable que voy a filtrar y que tipo voy a filtrar  -->
